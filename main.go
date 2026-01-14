23412115e34611841b3fcf6f56d6079b00ef7296
SheckHall <13789176472@163.com>
2026-01-14 23:44:31 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -1,83 +1,48 @@
-// 1(无缓冲区的channel)
-//package main
-//
-//import "fmt"
-//
-//func main() {
-//	c := make(chan string)
-//	go func() {
-//		fmt.Println("waiting for information...")
-//		s := <-c
-//		fmt.Println("received information:", s)
-//	}()
-//
-//	c <- "hello"
-//	fmt.Println("main_goroutine finished")
-//}
-
-// 2(带有缓冲区的channel的读写)
-//package main
-//
-//import (
-//	"fmt"
-//	"sync"
-//)
-//
-//func main() {
-//	ch := make(chan int, 5)
-//	wait := sync.WaitGroup{}
-//	wait.Add(1)
-//	go func() {
-//		defer wait.Done()
-//		ch <- 1
-//		ch <- 2
-//		ch <- 3
-//		//fmt.Println("阻塞恢复")
-//	}()
-//	wait.Wait()
-//	ch <- 5
-//	ch <- 6
-//	num := <-ch
-//	fmt.Println("num:", num)
-//	close(ch)
-//	for data := range ch {
-//		fmt.Println("data:", data)
-//	}
-//}
-
-// 3 工作池（simple）
 package main
 
 import (
+	"Goland/cmd/goTeam5/homework1/pool"
 	"fmt"
 	"sync"
 )
 
-type Job struct {
-	Do func(id int)
-}
+var result1, result2 int64 // 全局变量结果
+var lock sync.Mutex        // 全局互斥锁
 
+// 定义累加操作Task函数
+func Task1() {
+	lock.Lock() // 上锁，实现并发操作
+	defer lock.Unlock()
+	for range 100 {
+		result1++
+	}
+}
+func Task2() {
+	lock.Lock() // 上锁，实现并发操作
+	defer lock.Unlock()
+	for range 50 {
+		result2++
+	}
+}
 func main() {
-	channel := make(chan Job, 10)
-	waiter := sync.WaitGroup{}
-	waiter.Add(15)
-	for i := range 4 {
-		go func(id int) {
-			for job := range channel {
-				job.Do(id)
-				waiter.Done()
-			}
-		}(i) //这里必须要让i作为值拷贝，传入这个匿名函数  相关知识点：闭包陷阱
-	}
-	for i := range 15 {
-		j := i
-		job := Job{
-			Do: func(id int) {
-				fmt.Printf("【工作编号：%d，执行任务%d】\n", id, j)
-			},
-		}
-		channel <- job
-	}
-	close(channel)
-	waiter.Wait()
+	var tasknum int
+	var goroutines int
+	fmt.Print("输入累加任务数目：")
+	fmt.Scan(&tasknum)
+	fmt.Print("输入协程数目：")
+	fmt.Scan(&goroutines)
+
+	mypool1 := pool.NewPool()
+	mypool1.Put(Task1, tasknum)
+	mypool1.Arrange(goroutines)
+	mypool1.Wait()
+	fmt.Println("Task1:", result1)
+
+	fmt.Println()
+
+	mypool2 := pool.NewPool()
+	mypool2.Put(Task2, tasknum)
+	mypool2.Arrange(goroutines)
+	mypool2.Wait()
+	fmt.Println("Task2:", result2)
 }
